fa527413542edc32379902be4fd88f4fbaa21fb1
ono5 <kuroneko_head_lord@yahoo.co.jp>
2019-06-16 08:17:57 +0900

    feat: Added Class Base View

--- first/demo/urls.py
+++ first/demo/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
+from .views import Another
 
 urlpatterns = [
-    path('', views.first, name='first'),
+    path('first', views.first, name='first'),
+    path('another', Another.as_view(), name='another'),
 ]
--- first/demo/views.py
+++ first/demo/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views import View
+
+
+class Another(View):
+
+    def get(self, request):
+        return HttpResponse("This is another function inside class")
 
 
 def first(request):
