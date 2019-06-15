3a2fb72cc6f8d1df440a6ac5ac42f94b7447b014
ono5 <kuroneko_head_lord@yahoo.co.jp>
2019-06-16 08:39:32 +0900

    feat: Added Model Function

--- first/demo/models.py
+++ first/demo/models.py
@@ -2,19 +2,9 @@ from django.db import models
 
 
 class Book(models.Model):
-    STATUS = (
-        (0, 'Hobbit'),
-        (1, 'Lord of the ring'),
-        (2, 'paid')
-    )
-    title = models.CharField(max_length=36,
-                             null=True,
-                             blank=True,
-                             unique=True,
-                             default='',
-                             choices=STATUS)
+    title = models.CharField(max_length=36)
     description = models.TextField(max_length=256, blank=True)
-    price = models.DecimalField(default=0, decimal_places=2, max_digits=2)
+    price = models.IntegerField(default=0)
     published = models.DateField(blank=True, null=True, default=None)
     is_published = models.BooleanField(default=False)
     cover = models.ImageField(upload_to='covers/', blank=True)
--- first/demo/views.py
+++ first/demo/views.py
@@ -1,13 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views import View
+from .models import Book
 
 
 class Another(View):
 
-    def get(self, request):
-        return HttpResponse("This is another function inside class")
+    books = Book.objects.all()
+    # books = Book.objects.filter(is_published=False)
+    output = ''
 
+    for book in books:
+        output += f"<p>We have {book.title} books with ID {book.id}</p>"
+
+    def get(self, request):
+        return HttpResponse(self.output)
 
 def first(request):
     return HttpResponse("First message from views")
